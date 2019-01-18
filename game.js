60a646e192aeb06720fb44e81b4ae6bade5561ea
niulei <rainbownl@163.com>
2019-01-18 18:43:53 +0800

    提交代码

--- game.js
+++ game.js
@@ -5,7 +5,116 @@ import Main from './js/main'
 
 new Main()*/
 
-var context = wx.createCanvas()
+function Snake(in_maxNodes){
+    this.maxNodes = in_maxNodes
+    this.nodes = new Array(this.maxNodes)
+    this.length = 0
+    this.direction = DIRECTION_UP
+    for (var i = 0; i < this.maxNodes; i++) {
+        this.nodes[i] = new Point()
+    }
+
+    this.Add = function(in_x, in_y){
+        if (this.length >= this.maxNodes) {
+            return
+        }
+        this.nodes[this.length].x = in_x
+        this.nodes[this.length].y = in_y
+        this.length++
+    }
+
+    this.Remove = function(idx){
+        if (idx < 0 || idx >= this.length){
+            return
+        }
+        if (idx < this.length - 1){
+            for (var i = idx; i < this.length-1; i++){
+                this.nodes[i].x = this.nodes[i+1].x
+                this.nodes[i].y = this.nodes[i+1].y
+            }
+        }
+
+        this.nodes[length-1].x = 0
+        this.nodes[length-1].y = 0
+        length --
+    }
+}
+
+function Point(){
+    this.x = 0
+    this.y = 0
+}
+
+const DIRECTION_UP = 0
+const DIRECTION_LEFT = 1
+const DIRECTION_BOTTOM = 2
+const DIRECTION_RIGHT = 3
+
+var canvas = wx.createCanvas()
+var context = canvas.getContext("2d")
+//context.fillStyle = 'red'
+//context.fillRect(0, 0, 100, 100)
+var mStartX = 10
+var mStartY = 10
+var mPanelWidth = canvas.width - 20
+var mPanelHeight = canvas.height - 20
+var mNumBlockW = 20
+var mNumBlockH = 40
+var mBlockWidth = 0
+var mBlockColor = 'black'
+var mBackgroundColor = 'white'
+
+function initGrids(){
+    var block_w = mPanelWidth / mNumBlockW
+    var block_h = mPanelHeight / mNumBlockH
+    mBlockWidth = Math.min(block_w, block_h)
+}
+
+function drawGrids(){
+    context.lineStyle = mBlockColor
+    for(var i = 0; i <= mNumBlockW; i++){
+        context.moveTo(mStartX + i * mBlockWidth, mStartY)
+        context.lineTo(mStartX + i * mBlockWidth, mStartY + mNumBlockH * mBlockWidth)
+        context.stroke()
+    }
+    
+    for(var i = 0; i <= mNumBlockH; i++){
+        context.moveTo(mStartX, mStartY + i * mBlockWidth)
+        context.lineTo(mStartX + mNumBlockW * mBlockWidth, mStartY + i * mBlockWidth)
+        context.stroke()
+    }
+}
+
+function drawBlock(x, y){
+    context.fillStyle = mBlockColor
+    context.fillRect(mStartX + x * mBlockWidth, mStartY + y * mBlockWidth, mBlockWidth, mBlockWidth)
+}
+
+function drawSnake(snake){
+    for (var i = 0; i < snake.length; i++){
+        drawBlock(snake.nodes[i].x, snake.nodes[i].y)
+    }
+}
+
+function main(){
+    //draw background color
+    context.fillStyle = mBackgroundColor
+    context.fillRect(0, 0, canvas.width, canvas.height)
+
+    var snake = new Snake(100)
+
+    snake.Add(5, 6)
+    snake.Add(5, 7)
+    initGrids()
+    drawGrids()
+    drawSnake(snake)
+}
+
+main()
+
+wx.onTouchMove(function(res){
+
+})
 /*var objects = new Array(20)
 var objectCount = 0
 var background_color = '#243044'
